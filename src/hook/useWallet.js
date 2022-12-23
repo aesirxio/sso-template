0afaa4f8271b6558ea5e5e2370bf81cc376b9739
Lu Nguyen <lu@r-digital.tech>
2022-12-23 17:18:48 +0700

    recirect_uri

--- src/hook/useWallet.js
+++ src/hook/useWallet.js
@@ -76,63 +76,12 @@ const useWallet = (wallet, publicAddress) => {
 
       const { data } = await axios(config);
 
-      // Get member
-      const getUser = BaseRoute.__createRequestURL(
-        {
-          webserviceClient: "site",
-          webserviceVersion: "1.0.0",
-          option: "persona",
-          task: "getTokenByUser",
-          api: "hal",
-          access_token: data?.result?.access_token,
-        },
-        false,
-        urlPOST
-      );
-
-      const response = await axios({
-        method: "get",
-        url: getUser,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      console.log("122", response?.data?.result?.member_id);
-
-      if (response?.data?.result?.member_id) {
-        // Get profile
-        const getProfile = BaseRoute.__createRequestURL(
-          {
-            webserviceClient: "site",
-            webserviceVersion: "1.0.0",
-            option: "member",
-            api: "hal",
-            id: response?.data?.result?.member_id,
-            access_token: data?.result?.access_token,
-          },
-          false,
-          urlPOST
-        );
-
-        const responseProfile = await axios({
-          method: "get",
-          url: getProfile,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-
-        if (responseProfile?.data?.licenses) {
-          const sso = responseProfile?.data?.licenses.find(
-            (e) => e.subscription[0]?.product === "product-aesirx-sso"
-          );
-          console.log("sso", sso.data.domain);
-
-          window.location.href = `http://${
-            sso.data.domain
-          }?state=sso&${queryString.stringify(data.result)}&lastVisitDate=0`;
-        }
+      if (data?.result?.recirect_uri) {
+        window.location.href = `http://${
+          data?.result?.recirect_uri
+        }?state=sso&${queryString.stringify(data.result)}&lastVisitDate=0`;
+      } else {
+        throw false;
       }
     } catch (error) {
       toast("Your wallet is not registered");
