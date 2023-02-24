2caae8f7041fde2ab71aa16ac591cad115c58927
vladimir <vladimir@r-digital.tech>
2023-02-24 11:48:42 +0200

    Add specific wallet requests to the SSO configuration

--- src/provider/index.js
+++ src/provider/index.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import Metamask from './metamask';
-import { WagmiConfig, createClient } from 'wagmi';
-import { getDefaultProvider } from 'ethers';
+import {WagmiConfig, createClient} from 'wagmi';
+import {getDefaultProvider} from 'ethers';
 import Concordium from './concordium';
 import ToastComponent from '../components/Toast';
 
 const client = createClient({
-  autoConnect: true,
-  provider: getDefaultProvider(),
+    autoConnect: true,
+    provider: getDefaultProvider(),
 });
 
-const ProviverLogin = () => {
-  return (
-    <>
-      <div className="control-group">
-        <WagmiConfig client={client}>
-          <Metamask />
-        </WagmiConfig>
-      </div>
-      <div className="control-group">
-        <Concordium />
-      </div>
-      <ToastComponent />
-    </>
-  );
+const ProviderLogin = () => {
+
+    const urlParams = new URLSearchParams(window.location.search);
+    let login = ["concordium", "metamask", "regular"];
+
+    if (urlParams.has('return')) {
+        login = (new URL(atob(urlParams.get('return')))).searchParams.getAll("login[]")
+    }
+
+    const hasMetamask = login.includes("metamask");
+    const hasConcordium = login.includes("concordium");
+
+    return (
+        <>
+            {hasMetamask
+                ? <div className="control-group">
+                    <WagmiConfig client={client}>
+                        <Metamask/>
+                    </WagmiConfig>
+                </div>
+                : ''
+            }
+
+            {hasConcordium
+                ? <div className="control-group">
+                    <Concordium/>
+                </div>
+                : ''
+            }
+            <ToastComponent/>
+        </>
+    );
 };
 
-export default ProviverLogin;
+export default ProviderLogin;
