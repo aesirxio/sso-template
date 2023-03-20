e797253fa53d73efdc079c119368ec73d571394e
Viet Nguyen <viet@r-digital.tech>
2023-03-20 17:16:55 +0700

    Connect Status

--- src/provider/concordium/sign.js
+++ src/provider/concordium/sign.js
@@ -5,15 +5,17 @@ import { toast } from 'react-toastify';
 import { shortenString } from '../../utils/shortenString';
 const SignMessageConcordium = ({ account, connection }) => {
   const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState('');
   const wallet = 'concordium';
 
   const { getWalletNonce, verifySignature } = useWallet(wallet, account);
 
   const handleConnect = async () => {
-    setLoading(true);
+    setStatus('connect');
     const nonce = await getWalletNonce();
     if (nonce) {
       try {
+        setStatus('sign');
         const signature = await connection.signMessage(account, `${nonce}`);
         let convertedSignature =
           typeof signature === 'object' && signature !== null ? signature : JSON.parse(signature);
@@ -25,8 +27,7 @@ const SignMessageConcordium = ({ account, connection }) => {
         toast(error.message);
       }
     }
-
-    setLoading(false);
+    setStatus('');
   };
   return (
     <>
@@ -38,9 +39,25 @@ const SignMessageConcordium = ({ account, connection }) => {
         className="btn btn-secondary bg-secondary fw-semibold text-white"
         onClick={handleConnect}
       >
-        <img className="me-2" width={20} height={21} src={logo} alt="logo-concordium" />
-
-        {loading ? 'Waiting...' : 'Sign in via Concordium'}
+        {status ? (
+          <div className="d-flex align-items-center">
+            <span
+              className="spinner-border spinner-border-sm me-1"
+              role="status"
+              aria-hidden="true"
+            ></span>
+            <span className="ms-1">
+              {status === 'sign'
+                ? 'Please Sign message on the wallet'
+                : `Please wait to connect...`}
+            </span>
+          </div>
+        ) : (
+          <>
+            <img className="me-2" width={20} height={21} src={logo} alt="logo-concordium" />
+            Sign in via Concordium
+          </>
+        )}
       </button>
     </>
   );
